2a42f8daaf62af5cbf9bcf211fc95ec38167d86b
Carlos Murillo <carlos.murillo@abrigo.com>
2022-04-11 18:46:29 -0600

    Renamed Build.bat for Start.bat, Modified Build.ps1 file with common tasks once building app for first time (work on progress).

--- Build.ps1
+++ Build.ps1
@@ -11,50 +11,52 @@ $FrontEndProject = ".\CentralPerk.frontend"
 $MigrationsPath = ".\CentralPerk.Data\Migrations"
 
 # Access Solution Path.
-WRITE-HOST "Build Execution STARTED. `n" -ForegroundColor Yellow
+WRITE-HOST "`nBuild Execution STARTED." -ForegroundColor Yellow
 CD $SolutionPath
 
 # Define Environment Variables ONLY for execution.
-# WRITE-HOST "Define Environment Variables. `n" -ForegroundColor Green
+WRITE-HOST "`nDefine Environment Variables." -ForegroundColor Green
 # $env:PATH += ";$SolutionPath\node_modules\.bin\"
-$env:PGPASSWORD='root'
+$env:PGPASSWORD=$PGPassword
 
 # Start Services.
-WRITE-HOST "Running 'POSTGRES SQL' Service. `n" -ForegroundColor Green
+WRITE-HOST "`nRunning 'POSTGRES SQL' Service." -ForegroundColor Green
 SET-SERVICE -NAME $PGService -STATUS "Running" -STARTUPTYPE manual
 
 # Create a Tools Manifest, this will define execution tools just locally.
-WRITE-HOST "Creating Tools Manifest locally. `n" -ForegroundColor Green
+WRITE-HOST "`nCreating Tools Manifest locally." -ForegroundColor Green
 DOTNET new tool-manifest --force
 
 # Install DotNet Entity Framework will be installed locally.
-WRITE-HOST "Installing .NET Entity Framework locally. `n" -ForegroundColor Green
+WRITE-HOST "`n`nInstalling .NET Entity Framework locally." -ForegroundColor Green
 DOTNET tool install dotnet-ef
 
 # PSQL must be a environment variable, UserName 'postgres' and Password 'root' needs to be created by default.
-WRITE-HOST "Creating DB. `n" -ForegroundColor Green
-PSQL -U $PGUser -f $CreateScript
+# WRITE-HOST "`nCreating DB." -ForegroundColor Green
+# PSQL -U $PGUser -f $CreateScript
 
 # Add Migrations.
-WRITE-HOST "Add Migrations. `n" -ForegroundColor Green
+WRITE-HOST "`nAdd Migrations." -ForegroundColor Green
 IF (TEST-PATH $MigrationsPath) {
-    Remove-Item -Path $MigrationsPath -Force -Recurse
+    Remove-Item -Path $MigrationsPath -Recurse
 }
 
-DOTNET dotnet-ef -p $DataProject -s $WebProject Migrations Add FullMigrations
+DOTNET dotnet-ef -p $DataProject -s $WebProject Migrations Add CompleteMigration
 
 # Update DataBase.
-WRITE-HOST "Update DataBase. `n" -ForegroundColor Green
-DOTNET dotnet-ef -p $DataProject -s $WebProject Database Update
+WRITE-HOST "`nUpdate DataBase." -ForegroundColor Green
+# DOTNET dotnet-ef -p $DataProject -s $WebProject Database Update
 
 # Insert SQL Data.
-WRITE-HOST "Insert TEST Data. `n" -ForegroundColor Green
-PSQL -U $PGUser -d $DBName -f $InsertsScript
+# WRITE-HOST "`nInsert TEST Data." -ForegroundColor Green
+# PSQL -U $PGUser -d $DBName -f $InsertsScript
 
 # Install NPM Packages.
-WRITE-HOST "Installing NPM Packages. `n" -ForegroundColor Green
-# NPM I $FrontEndProject
+WRITE-HOST "`nInstalling NPM Packages." -ForegroundColor Green
+CD $FrontEndProject
+NPM I
+CD ..
 
 # Close PS Console.
-WRITE-HOST "Build Execution COMPLETED. `n" -ForegroundColor Yellow
+WRITE-HOST "`nBuild Execution COMPLETED.`n" -ForegroundColor Yellow
 EXIT
--- Start.bat
+++ Start.bat
@@ -1,2 +1,2 @@
-:: START PS Admin.
+:: Initialize PS with Admin privileges.
 POWERSHELL START-PROCESS POWERSHELL -VERB runAs -ArgumentList '-noexit', '-File', '%CD%\Build.ps1 %CD%'
